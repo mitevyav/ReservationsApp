49c4f39b1bfd3ec5f252e97b3a975fb37c3cfc69
ymitev <yavor.mitev@securax.org>
2017-07-11 00:08:27 +0300

    Add menu for the AddReservation activity.  Make the "add reservation" action button to call the addReservation() method.

--- app/src/main/java/com/example/yavor/reservations/AddReservation.java
+++ app/src/main/java/com/example/yavor/reservations/AddReservation.java
@@ -2,6 +2,9 @@ package com.example.yavor.reservations;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AddReservation extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class AddReservation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reservation);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_reservation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_reservation:
+                addReservation();
+                finish();
+                return false;
+        }
+
+        return (super.onOptionsItemSelected(item));
+    }
+
+    private void addReservation() {
+
     }
 }
