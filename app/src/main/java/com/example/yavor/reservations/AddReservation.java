1d6618028dc5f2f72257489f299a180040b66a81
ymitev <yavor.mitev@securax.org>
2017-07-10 23:42:44 +0300

    Add AddReservation activity. Add fab button in the main activity and start the AddReservation activity.

---
+++ app/src/main/java/com/example/yavor/reservations/AddReservation.java
@@ -0,0 +1,13 @@
+package com.example.yavor.reservations;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddReservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reservation);
+    }
+}
--- app/src/main/java/com/example/yavor/reservations/MainActivity.java
+++ app/src/main/java/com/example/yavor/reservations/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.yavor.reservations;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String LOG_TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.fab).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab:
+                startAddReservationActivity();
+                break;
+        }
+    }
+
+    private void startAddReservationActivity() {
+        Intent intent = new Intent(this, AddReservation.class);
+        startActivity(intent);
+    }
+
 }
