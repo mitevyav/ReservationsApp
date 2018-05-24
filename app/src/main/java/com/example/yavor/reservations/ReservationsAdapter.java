5535c15f225d659eb5a8326a67f5a6ef96b56757
ymitev <yavor.mitev@securax.org>
2018-05-24 13:40:01 +0300

    Change ReservationsAdapter to use the reservation list.

--- app/src/main/java/com/example/yavor/reservations/ReservationsAdapter.java
+++ app/src/main/java/com/example/yavor/reservations/ReservationsAdapter.java
@@ -1,14 +1,15 @@
 package com.example.yavor.reservations;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.yavor.reservations.data.ReservationsContract.ReservationEntry;
+import com.example.yavor.reservations.data.model.Reservation;
+
+import java.util.List;
 
 /**
  * Created by mitevyav on 10.7.2017 г..
@@ -19,22 +20,16 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
 
     private Context context;
 
-    private Cursor cursor;
+
+    private List<Reservation> reservations;
 
     public ReservationsAdapter(Context context) {
         this.context = context;
     }
 
-    public void swapCursor(Cursor newCursor) {
-        if (this.cursor == newCursor) {
-            return;
-        }
-
-        this.cursor = newCursor;
-
-        if (newCursor != null) {
-            notifyDataSetChanged();
-        }
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -47,15 +42,13 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
     @Override
     public void onBindViewHolder(ReservationViewHolder holder, int position) {
 
-        if (!cursor.moveToPosition(position)) {
-            return;
-        }
+        Reservation reservation = reservations.get(position);
 
 
-        String name = cursor.getString(cursor.getColumnIndex(ReservationEntry.COLUMN_GUEST_NAME));
-        int numberOfGuests = cursor.getInt(cursor.getColumnIndex(ReservationEntry.COLUMN_NUMBER_OF_GUESTS));
-        String timestamp = cursor.getString(cursor.getColumnIndex(ReservationEntry.COLUMN_TIMESTAMP));
-        int id = cursor.getInt(cursor.getColumnIndex(ReservationEntry._ID));
+        String name = reservation.getGuestName();
+        int numberOfGuests = reservation.getGuestsCount();
+        String timestamp = reservation.getDate().toString();
+        int id = reservation.getId();
 
         holder.name.setText(context.getString(R.string.guest_name_label, name));
         holder.guestNumber.setText(context.getString(R.string.number_of_guests_label, numberOfGuests));
@@ -66,10 +59,10 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
 
     @Override
     public int getItemCount() {
-        if (cursor == null) {
+        if (reservations == null) {
             return 0;
         }
-        return cursor.getCount();
+        return reservations.size();
     }
 
     class ReservationViewHolder extends RecyclerView.ViewHolder {
@@ -81,9 +74,9 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
         public ReservationViewHolder(View itemView) {
             super(itemView);
 
-            name = (TextView) itemView.findViewById(R.id.name);
-            guestNumber = (TextView) itemView.findViewById(R.id.number_of_guests);
-            timeStamp = (TextView) itemView.findViewById(R.id.timestamp);
+            name = itemView.findViewById(R.id.name);
+            guestNumber = itemView.findViewById(R.id.number_of_guests);
+            timeStamp = itemView.findViewById(R.id.timestamp);
         }
     }
 }
