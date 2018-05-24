00112f7031c1ff263df1dc75d221d88fbbc7b6a3
ymitev <yavor.mitev@securax.org>
2018-05-24 15:49:50 +0300

    Extend ReservationApp.java

---
+++ app/src/main/java/com/example/yavor/reservations/ReservationApp.java
@@ -0,0 +1,10 @@
+package com.example.yavor.reservations;
+
+import android.app.Application;
+
+public class ReservationApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
